3ca94749339a61e7d3bbe33680c80a3e4f218197
TLiuCaltech <Timothy@MacBook-Pro-5.local>
2018-05-09 18:30:34 -0700

    5/9/18

--- playground.py
+++ playground.py
@@ -4,7 +4,9 @@
 # 05/03/18    Tim Liu    started file; wrote cart_pole
 
 import gym
+from gym_extensions.continuous import gym_navigation_2d
 from gym_extensions.continuous import mujoco
+
 
 
 def cart_pole():
@@ -13,6 +15,21 @@ def cart_pole():
     for _ in range(1000):
         env.render()
         env.step(env.action_space.sample()) # take a random action
+    print("How'd I do?")
+    return
+
+def cart_pole2():
+    env = gym.make('CartPole-v0')
+    for i_episode in range(20):
+        observation = env.reset()
+        for t in range(100):
+            env.render()
+            print(observation)
+            action = env.action_space.sample()
+            observation, reward, done, info = env.step(action)
+            if done:
+                print("Episode finished after {} timesteps".format(t+1))
+                break  
     print("How'd I do?")
     return
 
