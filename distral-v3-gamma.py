bae91f1ff819268158a6bb729a908a9a383585c2
Bianca Yang <byang@caltech.edu>
2018-05-31 18:30:56 -0700

    added distillation runs

--- distral-v3-gamma.py
+++ distral-v3-gamma.py
@@ -6,10 +6,9 @@
 # USAGE: --env ['env1', ..] If nothing passed, assumes 'InvertedPendulum-v2' and
 # 'HalfInvertedPendulum-v0'
 #
-# Revision History:
-# Tim Liu    05/31/18    updated for record keeping
-# Tim Liu    05/31/18    removed manual seeding
 #
+
+
 
 
 import argparse
@@ -18,9 +17,6 @@ import gym
 import numpy as np
 from itertools import count
 from collections import namedtuple
-
-# used for recording run time data (FOR_RECORD)
-import visualize
 
 import torch
 import torch.nn as nn
@@ -35,10 +31,10 @@ from hard_param import weights_init, normalized_columns_initializer
 parser = argparse.ArgumentParser(description='PyTorch actor-critic example')
 parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
                     help='discount factor (default: 0.99)')
-parser.add_argument('--alpha', type=float, default=0.9, metavar='A',
-                    help='alpha (default: 0.8)')
-parser.add_argument('--beta', type=float, default=0.1, metavar='B',
-                    help='beta (default: 0.2)')
+parser.add_argument('--alpha', type=float, default=0.7, metavar='A',
+                    help='alpha (default: 0.9)')
+parser.add_argument('--beta', type=float, default=0.3, metavar='B',
+                    help='beta (default: 5.)')
 parser.add_argument('--seed', type=int, default=543, metavar='N',
                     help='random seed (default: 543)')
 parser.add_argument('--render', action='store_true',
@@ -48,11 +44,6 @@ parser.add_argument('--log-interval', type=int, default=10, metavar='N',
 parser.add_argument('--envs', action='append', nargs='+', type=str)
 
 args = parser.parse_args()
-
-# global lists for recording run time behavior - initialized by init_list
-# (FOR_RECORD)
-length_records = []    # length of each run for each episode
-rr_records = []        # running rewards for each episode
 
 
 num_envs = 0
@@ -64,18 +55,21 @@ if args.envs:
 pi = Variable(torch.FloatTensor([math.pi]))
 
 if num_envs == 0:
-    envs_names = ['InvertedPendulum-v2', 'LongInvertedPendulum-v0']
+    envs_names = ['InvertedPendulum-v2', 'HalfInvertedPendulum-v0']
     # first environment
     env1 = gym.make('InvertedPendulum-v2')
     # second environment
-    env2 = gym.make('LongInvertedPendulum-v0')
+    env2 = gym.make('HalfInvertedPendulum-v0')
     num_envs = 2
     envs = [env1, env2]
 else:
     envs = [gym.make(envs_names[i]) for i in range(num_envs)]
 
+for env in envs:
+    env.seed(args.seed)
 
 
+torch.manual_seed(args.seed)
 
 class Policy(nn.Module):
     def __init__(self):
@@ -106,10 +100,60 @@ class Policy(nn.Module):
 
 
         # initialize lists for holding run information
-        #self.div = [[] for i in range(self.num_envs)]
+        self.div = [[] for i in range(self.num_envs)]
+        self.saved_actions = [[] for i in range(self.num_envs)]
+        self.entropies = [[] for i in range(self.num_envs)]
+        self.rewards = [[] for i in range(self.num_envs)]
+        self.log_prob = [[] for i in range(self.num_envs)]
+
+    def forward(self, y, env_idx):
+        '''updated to have 5 return values (2 for each action head one for
+        value'''
+        x = F.relu(self.affine1(y))
+        mu = self.mu_heads[env_idx](x)
+        sigma2 = self.sigma2_heads[env_idx](x)
+        sigma = F.softplus(sigma2)
+        value = self.value_heads[env_idx](x)
+
+        z = F.relu(self.affine_pi0(y))
+        mu_dist = self.mu_heads[num_envs](z)
+        sigma2_dist = self.sigma2_heads[num_envs](z)
+        sigma_dist = F.softplus(sigma2_dist)
+        return mu, sigma, value, mu_dist, sigma_dist
+
+class Policy(nn.Module):
+    def __init__(self):
+        super(Policy, self).__init__()
+        self.num_envs = num_envs
+        # shared layer
+        self.affine1 = nn.Linear(4, 128)
+        self.affine_pi0 = nn.Linear(4, 128)
+        # not shared layers
+        self.mu_heads = nn.ModuleList([nn.Linear(128, 1) for i in
+                                       range(self.num_envs+1)])
+        self.sigma2_heads = nn.ModuleList([nn.Linear(128, 1) for i in
+                                           range(self.num_envs+1)])
+        self.value_heads = nn.ModuleList([nn.Linear(128, 1) for i in range(self.num_envs)])
+
+        self.apply(weights_init)
+        # +1 for the distilled policy
+        for i in range(self.num_envs+1):
+            mu = self.mu_heads[i]
+            sigma = self.sigma2_heads[i]
+            mu.data = normalized_columns_initializer(mu.weight.data, 0.01)
+            mu.bias.data.fill_(0)
+            sigma.bias.data.fill_(0)
+            if i != self.num_envs:
+                value = self.value_heads[i]
+                value.weight.data = normalized_columns_initializer(value.weight.data, 1.0)
+                value.bias.data.fill_(0)
+
+
+        # initialize lists for holding run information
+        self.div = [[] for i in range(self.num_envs)]
         self.saved_actions = [[] for i in range(self.num_envs)]
         #self.entropies = [[] for i in range(num_envs)]
-        #self.entropies = [[] for i in range(self.num_envs)]
+        self.entropies = [[] for i in range(self.num_envs)]
         self.rewards = [[] for i in range(self.num_envs)]
         self.log_prob = [[] for i in range(self.num_envs)]
         self.kl = []
@@ -167,12 +211,12 @@ def select_action(state, env_idx):
     log_prob = prob.log_prob(action)
     #log_prob_pi0 = -torch.div((action - mu_t).pow(2), 2*sigma_t) - 1/2*(2*pi*sigma_t).log()
     model.saved_actions[env_idx].append(SavedAction(log_prob, value))
-    #model.entropies[env_idx].append(entropy)
+    model.entropies[env_idx].append(entropy)
 
 
-    #model.div[env_idx].append(torch.div(sigma_t.sqrt(), args.alpha*sigma_t.sqrt()+ args.beta*sigma.sqrt()).log() + \
-    #         (args.alpha*sigma_t.sqrt()+args.beta*sigma.sqrt()).pow(2) + \
-    #         torch.div(((1-args.alpha)*mu_t-(args.beta)*mu).pow(2),(2*sigma_t)) - 0.5)
+    model.div[env_idx].append(torch.div(sigma_t.sqrt(), args.alpha*sigma_t.sqrt()+ args.beta*sigma.sqrt()).log() + \
+             (args.alpha*sigma_t.sqrt()+args.beta*sigma.sqrt()).pow(2) + \
+             torch.div(((1-args.alpha)*mu_t-(args.beta)*mu).pow(2),(2*sigma_t)) - 0.5)
     #model.log_prob[env_idx].append(log_prob_pi0)
     model.kl.append(torch.div(sigma_t.sqrt(), args.alpha*sigma_t.sqrt()+ args.beta*sigma.sqrt()).log() + \
              (args.alpha*sigma_t.sqrt()+args.beta*sigma.sqrt()).pow(2) + \
@@ -188,7 +232,6 @@ def finish_episode():
     entropy_sum = 0
     loss = torch.zeros(1, 1)
     loss = Variable(loss)
-    kl = []
 
     for env_idx in range(num_envs):
         saved_actions = model.saved_actions[env_idx]
@@ -212,9 +255,6 @@ def finish_episode():
         #    rewards = rewards + gamma * model.div[env_idx][i]
         #    gamma = gamma * args.gamma
 
-        kl = kl + list(map(lambda x, y: x *y, map(lambda i: args.gamma**(i[0]+1), enumerate(list(model.div[env_idx]))), list(model.dix[env_idx])))
-
-        temp = _model.div[env_idx]
         for (log_prob, value), r in zip(saved_actions, rewards):
             # reward is the delta param
             value += Variable(torch.randn(value.size()))
@@ -227,7 +267,9 @@ def finish_episode():
             # feeds a weird difference between value and the reward
             value_losses.append(F.smooth_l1_loss(value, torch.tensor([r])))
 
+    model.kl = list(map(lambda x, y: x *y, map(lambda i: args.gamma**(i[0]+1), enumerate(list(model.kl))), list(model.kl)))
 
+    # model.kl = list(map(lambda x: x[0][0] * x[1], zip(saved_actions, model.kl)))
 
     loss = (torch.stack(policy_losses).sum() + \
             0.5*torch.stack(value_losses).sum() + 0.002*torch.stack(model.kl).sum()- \
@@ -245,6 +287,7 @@ def finish_episode():
         print('grad')
         for i in range(1):
             #print(i)
+            print('1')
             print(model.mu_heads[num_envs].weight.grad)
             print(model.sigma2_heads[num_envs].weight.grad)
             print('##')
@@ -257,20 +300,15 @@ def finish_episode():
     # train the NN
     optimizer.step()
 
-    #model.div = [[] for i in range(model.num_envs)]
+    model.div = [[] for i in range(model.num_envs)]
     model.saved_actions = [[] for i in range(model.num_envs)]
-    #model.entropies = [[] for i in range(model.num_envs)]
+    model.entropies = [[] for i in range(model.num_envs)]
     model.rewards = [[] for i in range(model.num_envs)]
     model.log_prob = [[] for i in range(model.num_envs)]
     model.kl = []
     model.ent = []
 
 def main():
-
-    # initialize the record lists to the proper length (FOR_RECORD)
-    length_records = visualize.init_list(envs_names)
-    rr_records = visualize.init_list(envs_names)
-
     running_reward = 10
     run_reward = np.array([10 for i in range(num_envs)])
     roll_length = np.array([0 for i in range(num_envs)])
@@ -299,11 +337,6 @@ def main():
         # update our running reward
         running_reward = running_reward * 0.99 + length / num_envs * 0.01
         run_reward = run_reward * 0.99 + roll_length * 0.01
-
-        # call function to record run data (FOR_RECORD)
-        length_records, rr_records = visualize.update_records(\
-            roll_length, run_reward, length_records, rr_records)
-
         finish_episode()
         if i_episode % args.log_interval == 0:
             print('Episode {}\tAverage length per environment {}'.format(i_episode, run_reward))
@@ -323,9 +356,6 @@ def main():
 
     if i_episode == 5999:
         print('Well that failed')
-
-    visualize.pickle_list('distral', envs_names, length_records,\
-                              rr_records)
 
 if __name__ == '__main__':
     main()
